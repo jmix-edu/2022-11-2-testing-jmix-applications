73fec8cf777b31e0c58036f3199feb033cd49743
Roman Pinyazhin <pinyazhin@haulmont.com>
2022-11-30 14:14:06 +0400

    Add Masquerade UI model for BDD testing

---
+++ src/test/java/com/company/jmixpm/bdd/uimodel/MainScreen.java
@@ -0,0 +1,20 @@
+package com.company.jmixpm.bdd.uimodel;
+
+import com.company.jmixpm.screen.project.ProjectBrowse;
+import io.jmix.masquerade.Wire;
+import io.jmix.masquerade.base.Composite;
+import io.jmix.masquerade.component.SideMenu;
+
+public class MainScreen extends Composite<MainScreen> {
+
+    @Wire
+    private SideMenu sideMenu;
+
+    public ProjectBrowse openProjectBrowse() {
+        return sideMenu.openItem(new SideMenu.Menu<>(ProjectBrowse.class, "application", "Project.browse"));
+    }
+
+    public TaskBrowse openTaskBrowse() {
+        return sideMenu.openItem(new SideMenu.Menu<>(TaskBrowse.class, "application", "Task_.browse"));
+    }
+}
